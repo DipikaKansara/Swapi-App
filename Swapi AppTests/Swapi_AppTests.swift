b532d93aa60a8ac70647ed0633d35a33dcaa2e1c
Dipika Kansara <68623123+DipikaKansara@users.noreply.github.com>
2021-09-22 10:02:27 +1000

    Initial Commit

---
+++ Swapi AppTests/Swapi_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swapi_AppTests.swift
+//  Swapi AppTests
+//
+//  Created by Dipika Kansara on 22/9/21.
+//
+
+import XCTest
+@testable import Swapi_App
+
+class Swapi_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
